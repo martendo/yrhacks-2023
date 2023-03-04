1867455a64280ed721c74d23d18760c8f9b665d5
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-03-04 08:07:28 -0500

    add simple announcements

--- docs/script.js
+++ docs/script.js
@@ -21,8 +21,29 @@ for (const name of SECTIONS) {
 function loadAnnouncements() {
 	const req = new XMLHttpRequest();
 	req.addEventListener("load", () => {
-		document.getElementById("announcements").textContent = req.responseText;
+		let data = JSON.parse(req.responseText);
+		let announcements = document.getElementById("announcements");
+		for (const ann of data.reverse()) {
+			let box = document.createElement("div");
+			let title = document.createElement("h2");
+			title.textContent = ann.title
+			let date = document.createElement("p");
+			date.className = "datetime"
+			date.textContent = ann.date
+			let time = document.createElement("p");
+			time.className = "datetime"
+			time.textContent = ann.time
+			let content = document.createElement("p");
+			content.textContent = ann.content
+
+			box.appendChild(title);
+			box.appendChild(date);
+			box.appendChild(time);
+			box.appendChild(content);
+			announcements.appendChild(box);
+		}
 	});
+	// req.open("GET", "http://localhost:3000");
 	req.open("GET", "https://markham-yrhacks-2023.herokuapp.com");
 	req.send();
 }
--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const http = require("http");
 
-const announcements = [];
+const announcements = [
+	// ChatGPT wrote the announcements lol, can't be bothered
+	{
+		"date": "Jan 1, 2023",
+		"time": "12:00 AM",
+		"title": "Welcome to 2023!",
+		"content": "As of 2023, the new year has begun with a sense of hope and renewal. After two years of global upheaval due to the COVID-19 pandemic, people around the world are looking forward to a fresh start. While the pandemic is not yet over, the availability of vaccines and improved treatment options have brought a degree of stability and optimism. Many are making resolutions and setting goals for the year ahead, while others are simply grateful for the opportunity to spend time with loved ones and appreciate the small joys in life. As we move into 2023, there is a sense of unity and solidarity as people work together to overcome challenges and create a brighter future."
+	},
+	{
+		"date": "Feb 9, 2023",
+		"time": "5:35 PM",
+		"title": "Potential storm incoming, school closures?",
+		"content": "Due to the inclement weather forecasted for tomorrow, there is a possibility that schools may need to close for the day. The safety and well-being of students, faculty, and staff are of the utmost importance, and school officials will be closely monitoring weather conditions to make the best decision possible. In the event of a closure, students will be notified through various channels, such as social media, school websites, and local news outlets. Parents and guardians are encouraged to stay tuned to these sources for updates and to make alternative arrangements for their children if necessary. While a school closure can be inconvenient, it is important to prioritize safety during extreme weather conditions."
+	},
+	{
+		"date": "Mar 3, 2023",
+		"time": "6:00 PM",
+		"title": "YRHacks 2023",
+		"content": "Now, the event is starting! YRHacks 2023, the annual hackathon held in Markham, Ontario, by the York Region District School Board (YRDSB), has officially begun. Students from around the world are gearing up for a weekend of collaboration, learning, and innovation. With a focus on creativity and problem-solving, participants will work in teams to develop new projects in a variety of fields, from software development to artificial intelligence. The hackathon is an excellent opportunity for young people to challenge themselves, learn new skills, and connect with industry experts. Whether attending in-person or virtually, YRHacks 2023 is sure to be an exciting and rewarding experience for all involved."
+	}
+];
 
 function requestListener(req, res) {
 	console.log("Received request", req);
