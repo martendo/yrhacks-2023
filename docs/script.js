3f1c96bfa28e4a9e216d7fc8bb0071ad7f484427
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-03-04 08:32:33 -0500

    add announcement sorting

--- docs/script.js
+++ docs/script.js
@@ -18,23 +18,40 @@ for (const name of SECTIONS) {
 	document.getElementById(name + "Button").addEventListener("click", () => switchSection(name));
 }
 
+document.getElementById("announcementSortOrder").addEventListener("change", loadAnnouncements);
+document.getElementById("announcementRefresh").addEventListener("change", loadAnnouncements);
+
 function loadAnnouncements() {
 	const req = new XMLHttpRequest();
 	req.addEventListener("load", () => {
 		let data = JSON.parse(req.responseText);
 		let announcements = document.getElementById("announcements");
-		for (const ann of data.reverse()) {
+		let order = document.getElementById("announcementSortOrder");
+		announcements.innerHTML = "";
+
+		switch (order.options[order.selectedIndex].value) {
+			case "0":
+				data = data.reverse();
+				break;
+			case "1":
+				break;
+			case "2":
+				data = data.sort((a, b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0));
+				break;
+		}
+
+		for (const ann of data) {
 			let box = document.createElement("div");
 			let title = document.createElement("h2");
-			title.textContent = ann.title
+			title.textContent = ann.title;
 			let date = document.createElement("p");
-			date.className = "datetime"
-			date.textContent = ann.date
+			date.className = "datetime";
+			date.textContent = ann.date;
 			let time = document.createElement("p");
-			time.className = "datetime"
-			time.textContent = ann.time
+			time.className = "datetime";
+			time.textContent = ann.time;
 			let content = document.createElement("p");
-			content.textContent = ann.content
+			content.textContent = ann.content;
 
 			box.appendChild(title);
 			box.appendChild(date);
@@ -43,7 +60,7 @@ function loadAnnouncements() {
 			announcements.appendChild(box);
 		}
 	});
-	// req.open("GET", "http://localhost:3000");
-	req.open("GET", "https://markham-yrhacks-2023.herokuapp.com");
+	req.open("GET", "http://localhost:3000");
+	// req.open("GET", "https://markham-yrhacks-2023.herokuapp.com");
 	req.send();
 }
